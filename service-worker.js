ce30afe9adab8849e86e7138be6e7a82c1cbf530
matto <matto@damniam.de>
2017-10-01 01:02:07 +0200

    https, working offline cache and some minor optical changes

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,54 @@
-var cacheName = 'alf-v1';
-var filesToCache = [
-    "./img/bg.jpg",
-    "./img/apple-touch-icon.png",
-    "./img/favicon.ico",
-    "./img/favicon-192.png",
-    "./js/alf_quotes.js",
-    "./js/alf.js",
-    "./css",
-    "./css/alf.css",
+var alf_cache_name = 'alf';
+var alf_app_files = [
+    "/",
+    "/index.html",
+    "/img/bg.jpg",
+    "/img/apple-touch-icon.png",
+    "/img/favicon.ico",
+    "/img/favicon-192.png",
+    "/js/alf.js",
+    "/js/alf_quotes.js",
+    "/css/alf.css",
 ];
 
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
+        caches.open(alf_cache_name).then(function(cache) {
+            console.log('[ServiceWorker] Caching alf app');
+            return cache.addAll(alf_app_files);
         })
     );
 });
+
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+
+/*
+ * We need to increment alf_cache_name to remove old cached files...
+ */
+
+/*
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== alf_cache_name) {
+                console.log('[ServiceWorker] Removing old cache', key);
+                return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+*/
